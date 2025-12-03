23fd7335ec72cfae35fcfa060df91efa53ec2cc5
ramblehead <v.rybynok@gmail.com>
2025-12-03 01:37:20 +0000

    Adding PySide/PyQt dependencies

--- flake.nix
+++ flake.nix
@@ -39,8 +39,48 @@
 
         runtimeLibs = with pkgs; [
           stdenv.cc.cc
-          glibc # libc, ld-linux, ...
-          stdenv.cc.cc.lib # libstdc++, libgcc_s
+
+          # Qt libs needed by PySide6 / PyQt6
+          qt6.qtbase
+          qt6.qtwayland
+          qt6.qtdeclarative
+          qt6.qtsvg
+          qt6.qttools
+
+          # X11/XCB
+          xorg.libX11
+          xorg.libXext
+          xorg.libXrender
+          xorg.libXfixes
+          xorg.libXrandr
+          xorg.libXi
+          xorg.libXcursor
+          xorg.libXinerama
+
+          xorg.libxcb
+          xorg.xcbutil
+          xorg.xcbutilimage
+          xorg.xcbutilkeysyms
+          xorg.xcbutilwm
+          xorg.xcbutilcursor
+          xorg.xcbutilrenderutil
+
+          # MESA GL — REQUIRED for libGL.so.1
+          mesa
+          libglvnd
+
+          pkg-config
+          fontconfig
+          glib
+          freetype
+          zstd
+          libxkbcommon
+          wayland
+
+          # dbus
+
+          # glibc # libc, ld-linux, ...
+          # stdenv.cc.cc.lib # libstdc++, libgcc_s
           # zlib
           # openssl
           # libuuid
@@ -88,13 +128,25 @@
           #   pkgs.lib.strings.trim
           #   (builtins.readFile "${pkgs.stdenv.cc}/nix-support/dynamic-linker");
           NIX_LD = pkgs.stdenv.cc.bintools.dynamicLinker;
-
           NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath runtimeLibs;
-          # LD_LIBRARY_PATH = NIX_LD_LIBRARY_PATH;
+
+          # Qt plugin paths
+          QT_PLUGIN_PATH = pkgs.lib.concatStringsSep ":" [
+            "${pkgs.qt6.qtbase}/lib/qt6/plugins"
+            "${pkgs.qt6.qtwayland}/lib/qt6/plugins"
+          ];
+
+          QT_QPA_PLATFORM_PLUGIN_PATH = "${pkgs.qt6.qtbase}/lib/qt6/plugins/platforms";
+          QML2_IMPORT_PATH = "${pkgs.qt6.qtdeclarative}/lib/qt6/qml";
 
           shellHook = ''
+            export LD_LIBRARY_PATH="$NIX_LD_LIBRARY_PATH"
+
             echo "''$(${pkgs.python}/bin/python --version)"
             echo "Using NIX_LD=$NIX_LD"
+            echo "Using NIX_LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH"
+            echo "Using LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+            echo
 
             ${self.checks.${system}.pre-commit-check.shellHook}
           '';
